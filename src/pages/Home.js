d117e2c5d63f7f37e12c652bb2f9eb843c0494ba
chavdar <chavmom@gmail.com>
2022-12-10 17:28:06 +0200

    Changes to be committed: 	modified:   src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,13 @@ export default function Home() {
   const [users,setUsers] = useState([]);
 
   useEffect(()=>{
-    console.log("Code with Chavdar!");
+    loadUsers();
+  },[]);
 
-  });
+  const loadUsers=async()=>{
+    const result=await axios.get("http://localhost:8080/users");
+    console.log(result);
+  }
 
 
   return (
