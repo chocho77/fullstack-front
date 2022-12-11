6c6d9ba894ffe210334e99876c183b9eda0e219b
chavdar <chavmom@gmail.com>
2022-12-11 07:59:11 +0200

    Changes to be committed: 	modified:   src/users/AddUser.js

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -6,6 +6,29 @@ export default function AddUser() {
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Register User</h2>
+          <div className='mb-3'>
+            <label htmlFor="Name" className='form-label'>
+              Name
+            </label>
+            <input type={text} className="form-control" placeholder="Enter your name" name="name"/>
+
+          </div>
+          
+          <div className='mb-3'>
+            <label htmlFor="Username" className='form-label'>
+              Username
+            </label>
+            <input type={text} className="form-control" placeholder="Enter your username" name="username"/>
+            
+          </div>
+          
+          <div className='mb-3'>
+            <label htmlFor="Email" className='form-label'>
+              E-mail
+            </label>
+            <input type={text} className="form-control" placeholder="Enter your e-mail address" name="email"/>
+            
+          </div>
 
 
         </div>
