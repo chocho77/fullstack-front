7098470a8b7a01e729582182db70fa25e0b2a892
chavdar <chavmom@gmail.com>
2022-12-12 21:00:03 +0200

    Changes to be committed: 	modified:   src/users/ViewUser.js

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import {Link } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import {Link, useParams } from "react-router-dom";
 
 export default function ViewUser() {
+
+    const [user, setUser]=useState({
+        name:"",
+        username:"",
+        email:""
+
+    });
+
+    const {id} = useParams();
+
+    useEffect({
+        loadUser();
+    },[]);
+
+    const loadUser = async ()=> {
+        const result=await axios.get(`http://localhost:8080/user/${id}`);
+        setUser(result.data);
+
+    }
+
+
   return (
     <div className='container'>
       <div className='row'>
